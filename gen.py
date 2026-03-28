525b1fa3018d853550dc44904677d48eff23fe7c
StardustMaker <yerbaht@aliyun.com>
2026-03-28 23:47:07 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -15,9 +15,6 @@ edit_times = {}
 window_scale = 7
 confusion_times = 7
 split_number = 7
-normal_window = 28960
-bad_seq_increment = -10000
-use_md5_option = True
 
 def cleanup_edit_times(edit_times):
     while True:
@@ -52,6 +49,7 @@ def modify_window(pkt):
         if ip.haslayer(TCP):
             key = f"{ip.dst}_{ip[TCP].dport}"
 
+
             if ip[TCP].flags == "SA":
                 edit_times[key] = [time.time(), 1]
                 ip = clear_window_scale(ip)
@@ -59,6 +57,7 @@ def modify_window(pkt):
                 del ip[IP].chksum
                 del ip[TCP].chksum
                 pkt.set_payload(bytes(ip))
+
 
                 thread = threading.Thread(target=send_payloads, args=(ip, ))
                 thread.start()
@@ -68,34 +67,24 @@ def modify_window(pkt):
                 if edit_times[key][1] < split_number:
                     ip[TCP].window = window_size
                 else:
-                    ip[TCP].window = normal_window
+                    ip[TCP].window = 28960
                 edit_times[key][1] += 1
                 del ip[IP].chksum
                 del ip[TCP].chksum
                 pkt.set_payload(bytes(ip))
     except Exception as e:
+        # print(traceback.format_exc())
         pass
     pkt.accept()
 
 def send_payloads(ip):
     if confusion_times < 1:
         return
-    for i in range(1, confusion_times + 1):
+    for i in range(1,confusion_times+1):
         _win_size = window_size
         if i == confusion_times:
             _win_size = 65535
-        bad_ack = ip[TCP].seq + bad_seq_increment
-        tcp_options = [('WScale', window_scale)] + [('NOP', '')] * 5
-        if use_md5_option:
-            tcp_options = [('MD5', b'\x00' * 16)] + tcp_options
-        ack_packet = IP(src=ip.dst, dst=ip.src) / TCP(
-            sport=ip[TCP].dport, 
-            dport=ip[TCP].sport, 
-            flags="A", 
-            ack=bad_ack,
-            window=_win_size, 
-            options=tcp_options
-        )
+        ack_packet = IP(src=ip.dst, dst=ip.src) / TCP(sport=ip[TCP].dport, dport=ip[TCP].sport, flags="A", ack=ip[TCP].seq +i, window=_win_size, options=[('WScale', window_scale)] + [('NOP', '')] * 5)
         send(ack_packet, verbose=False)
 
 
@@ -104,30 +93,26 @@ def parsearg():
     global window_scale
     global confusion_times
     global split_number
-    global normal_window
-    global bad_seq_increment
-    global use_md5_option
-    parser = argparse.ArgumentParser(description='TCP Window Obfuscation Tool')
+    parser = argparse.ArgumentParser(description='Description of your program')
 
 
-    parser.add_argument('-q', '--queue', type=int, required=True, help='iptables Queue Num')
-    parser.add_argument('-w', '--window_size', type=int, default=17, help='TCP Window Size for handshake (default: 17)')
-    parser.add_argument('-s', '--window_scale', type=int, default=7, help='TCP Window Scale')
-    parser.add_argument('-c', '--confusion_times', type=int, default=7, help='Confusion packet count')
-    parser.add_argument('-n', '--split_number', type=int, default=7, help='Packets with small window (default: 7)')
-    parser.add_argument('-nw', '--normal_window', type=int, default=28960, help='Normal window size after handshake (default: 28960)')
-    parser.add_argument('-b', '--bad_seq', type=int, default=-10000, help='Bad sequence number increment (default: -10000, negative=backward)')
-    parser.add_argument('--no-md5', action='store_true', help='Disable TCP MD5 option in fake packets')
+    parser.add_argument('-q', '--queue', type=int, help='iptables Queue Num')
+    parser.add_argument('-w', '--window_size', type=int, help='Tcp Window Size')
+    parser.add_argument('-s', '--window_scale', type=int, help='Tcp Window Scale')
+    parser.add_argument('-c', '--confusion_times', type=int, help='confusion_times')
+    parser.add_argument('-n', '--split_number', type=int, help='Tcp Split Number')
 
     args = parser.parse_args()
+
+
+    if args.queue is None or args.window_size is None:
+        exit(1)
+
 
     window_size = args.window_size
     window_scale = args.window_scale
     confusion_times = args.confusion_times
     split_number = args.split_number
-    normal_window = args.normal_window
-    bad_seq_increment = args.bad_seq
-    use_md5_option = not args.no_md5
 
 
     return args.queue
